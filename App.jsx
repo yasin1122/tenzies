974cad835cf34997aca524c4ba2ab3d71f04c9f8
Yasin Cakal <whoisyasin@gmail.com>
2025-03-21 17:10:33 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,77 @@
+import { useState, useRef, useEffect } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+
+export default function App() {
+    const [dice, setDice] = useState(() => generateAllNewDice())
+    const [rollCount, setRollCount] = useState(0)
+    const buttonRef = useRef(null)
+
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
+        
+    useEffect(() => {
+        if (gameWon) {
+            buttonRef.current.focus()
+        }
+    }, [gameWon]) 
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+    
+    function rollDice() {
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+            setRollCount(prevCount => prevCount + 1)
+        } else {
+            setDice(generateAllNewDice())
+            setRollCount(0)
+        }
+    }
+
+    function hold(id) {
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
+
+    return (
+        <main>
+            {gameWon && <Confetti />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to <strong><em>FREEZE</em></strong> its value. {rollCount !== 0 && <strong>Number of Rolls: {rollCount}</strong>}</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
+        </main>
+    )
+}
